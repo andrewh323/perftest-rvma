d6867658d8dbf6a9ceba75cfd70c31e72b5d03f9
Andrew Heaton <andrewh8@narval3.narval.calcul.quebec>
2025-07-23 19:09:28 -0400

    Implemented IP-vaddr translation

--- src/rvma_mailbox_server.c
+++ src/rvma_mailbox_server.c
@@ -1,7 +1,7 @@
 /*
 Test if mailbox is set up correctly - DONE
 Test if connection is establised - DONE
-    - Assuming vaddr is prenegotiated and known
+    Test conversion of IP address into vaddr - DONE
 Test if rvma_write is successful
 */
 
@@ -10,21 +10,66 @@ Test if rvma_write is successful
 #include <string.h>
 #include <unistd.h>
 #include <rdma/rdma_cma.h>
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "rvma_mailbox_hashmap.h"
 #include "rvma_write.h"
 
+
+uint32_t get_server_ip(const char *iface_name) {
+    struct ifaddrs *ifaddr, *ifa;
+    uint32_t ip = 0;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        return 0;
+    }
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL || ifa->ifa_addr->sa_family != AF_INET) {
+            continue;
+        }
+        
+        if (strcmp(ifa->ifa_name, iface_name) == 0) {
+            struct sockaddr_in *sa = (struct sockaddr_in *)ifa->ifa_addr;
+            ip = ntohl(sa->sin_addr.s_addr);
+            break;
+        }
+    }
+    freeifaddrs(ifaddr);
+    return ip;
+}
+
+
+uint64_t construct_vaddr(uint16_t reserved, uint32_t ip_host_order, uint16_t port) {
+    uint64_t res = (uint64_t)reserved << 48 | ((uint64_t)ip_host_order << 16) | port;
+    return res;
+}
+
+
 int main(int argc, char **argv) {
+
     int port = 7471;
-    int vaddr = 135;
+    const char *iface_name = "ib0";
+    uint32_t ip_host_order = get_server_ip(iface_name); // Get IP for vaddr construction
+    struct in_addr addr;
+    uint16_t reserved = 0x0001; // Reserved 16 bits for vaddr structure
+    addr.s_addr = htonl(ip_host_order); // Convert to network byte order for printing
+    printf("IP address found: %s\n", inet_ntoa(addr));
+
     struct sockaddr_in server_addr;
     struct rdma_cm_event *event;
-    
     memset(&server_addr, 0, sizeof(server_addr));
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(port);
     server_addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces
+
+    // Construct virtual address
+    uint64_t vaddr = construct_vaddr(reserved, ip_host_order, port);
+    printf("Constructed virtual address: %" PRIu64 "\n", vaddr);
 
     // Calls newMailboxIntoHashmap, which calls setupMailbox, which gets the
     // key, bufferQueue, ec, rdma cm_id
@@ -36,7 +81,7 @@ int main(int argc, char **argv) {
 
     RVMA_Mailbox *mailboxPtr = searchHashmap(windowPtr->hashMapPtr, &vaddr);
     if (!mailboxPtr) {
-        fprintf(stderr, "Failed to get mailbox for vaddr = %d\n", vaddr);
+        fprintf(stderr, "Failed to get mailbox for vaddr = %" PRIu64 "\n", vaddr);
         return -1;
     }
 
@@ -53,6 +98,7 @@ int main(int argc, char **argv) {
     // Define protection domain
     printf("Defining protection domain for qp\n");
     struct ibv_pd *pd = ibv_alloc_pd(client_cm_id->verbs);
+    mailboxPtr->pd = pd;
     if (!pd) {
         perror("ibv_alloc_pd failed");
         return -1;
@@ -62,6 +108,12 @@ int main(int argc, char **argv) {
     printf("Defining memory region for qp\n");
     int mr_size = QUEUE_CAPACITY * sizeof(RVMA_Buffer_Entry*);
     struct ibv_mr *mr = ibv_reg_mr(pd, mailboxPtr->virtualAddress, mr_size, IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE);
+
+    mailboxPtr->cq = ibv_create_cq(client_cm_id->verbs, 16, NULL, NULL, 0);
+    if (!mailboxPtr->cq) {
+        perror("ibv_create_cq failed");
+        return -1;
+    }
 
     // Create QP
     struct ibv_qp_init_attr qp_attr = {
