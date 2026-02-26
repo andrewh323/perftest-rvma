ebe5834b7059db327bd5e4634bb3631ad13b9c3a
Andrew Heaton <102182355+andrewh323@users.noreply.github.com>
2026-02-25 20:14:20 -0500

    Added support for multiple clients (Blocking)

--- src/rvsocket_server_stream.c
+++ src/rvsocket_server_stream.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <rdma/rsocket.h>
 #include <arpa/inet.h>
+#include <ifaddrs.h>
 
 #include "rvma_socket.h"
 #include "rvma_write.h"
@@ -19,42 +20,63 @@ static inline uint64_t rdtsc(){
     return ((uint64_t)hi << 32) | lo;
 }
 
+uint32_t get_host_addr(const char *iface_name) {
+    struct ifaddrs *ifaddr, *ifa;
+    uint32_t ip = 0;
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        return 0;
+    }
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL || ifa->ifa_addr->sa_family != AF_INET) {
+            continue;
+        }
+        
+        if (strcmp(ifa->ifa_name, iface_name) == 0) {
+            struct sockaddr_in *sa = (struct sockaddr_in *)ifa->ifa_addr;
+            ip = ntohl(sa->sin_addr.s_addr);
+            break;
+        }
+    }
+    freeifaddrs(ifaddr);
+    return ip;
+}
+
 
 int main(int argc, char **argv) {
 	uint64_t start, end;
 	double cpu_ghz = get_cpu_ghz();
 	double elapsed_us;
 	uint16_t reserved = 0x0001;
-	int listen_fd, conn_fd1, conn_fd2;
-	struct sockaddr_in client_addr;
-	memset(&client_addr, 0, sizeof(client_addr));
+	struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+	int listen_fd;
+	int num_clients = 2;
+	int conn_fd[num_clients];
 
-	client_addr.sin_family = AF_INET;
-    client_addr.sin_port = htons(PORT);
-    if (inet_pton(AF_INET, argv[1], &client_addr.sin_addr) != 1) {
-        perror("inet_pton failed");
-        return -1;
-    }
-
-	client_addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces
-	uint32_t ip_host_order = ntohl(client_addr.sin_addr.s_addr);
-
-	uint64_t vaddr = constructVaddr(reserved, ip_host_order, PORT);
+	uint32_t host_ip = get_host_addr("ib0");
+	uint64_t vaddr = constructVaddr(reserved, host_ip, PORT);
 	printf("Constructed virtual address: %" PRIu64 "\n", vaddr);
+
+	addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = INADDR_ANY; // Bind to all interfaces
 
 	RVMA_Win *windowPtr = rvmaInitWindowMailbox(vaddr);
 
     listen_fd = rvsocket(SOCK_STREAM, vaddr, windowPtr);
 
 	// Bind address to socket
-	rvbind(listen_fd, (struct sockaddr *)&client_addr, sizeof(client_addr));
+	rvbind(listen_fd, (struct sockaddr *)&addr, sizeof(addr));
 
 	// Listen for incoming connections
 	rvlisten(listen_fd, 5);
 	printf("Server listening on port %d...\n", PORT);
 
 	int size = 10;
-	int num_sends = 1000;
+	int num_sends = 100;
 
 	char *messages[num_sends];
     for (int i = 0; i < num_sends; i++) {
@@ -63,38 +85,29 @@ int main(int argc, char **argv) {
         snprintf(messages[i], size, "Msg %d", i);
     }
 
-	// Accept a connection from client
-	conn_fd1 = rvaccept(listen_fd, NULL, NULL, windowPtr);
-	if (conn_fd1 < 0) {
-		perror("rvaccept failed");
-		return -1;
-	}
-	printf("Client 1 successfully connected!\n");
-
-	conn_fd2 = rvaccept(listen_fd, NULL, NULL, windowPtr);
-	if (conn_fd2 < 0) {
-		perror("rvaccept failed");
-		return -1;
-	}
-	printf("Client 2 successfully connected!\n");
-
-	for (int i = 0; i < num_sends; i++) {
-		// Receive data from client
-		uint64_t t2;
-		int ret = rvrecv(conn_fd1, &t2);
-		if (ret < 0) {
-			perror("Error receiving message");
+	for (int i = 0; i < num_clients; i++) {
+		// Accept a connection from client
+		conn_fd[i] = rvaccept(listen_fd, NULL, NULL, windowPtr);
+		if (conn_fd[i] < 0) {
+			perror("rvaccept failed");
+			return -1;
 		}
+		printf("Client %d successfully connected!\n", i+1);
+	}
 
-		ret = rvsend(conn_fd1, messages[i], size);
-		if (ret < 0) {
-			perror("Error sending message");
+	// While server is running, poll all clients and recv messages
+	// Currently rvrecv is blocking but maintains flow control
+	while (1) {
+		for (int c = 0; c < num_clients; c++) {
+			rvrecv(conn_fd[c], NULL);
+			rvsend(conn_fd[c], messages[c], size);
 		}
 	}
 	
 	// Close the connection
-	rclose(conn_fd1);
-	rclose(conn_fd2);
+	for (int i = 0; i < num_clients; i++) {
+		rclose(conn_fd[i]);
+	}
 	rclose(listen_fd);
 	return 0;
 }
